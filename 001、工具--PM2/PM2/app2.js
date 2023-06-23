a0e275cae1d7a1acf6f01a781663adaf45cc3875
李宥 <591071179@qq.com>
2023-06-23 18:59:47 +0800

    pm2

---
+++ 001、工具--PM2/PM2/app2.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const server = http.createServer()
+
+server.on('request', (req, res) => {
+    res.end('请求成功' + new Date().toLocaleTimeString())
+})
+server.listen(8002, () => {
+    console.log('server running at http://127.0.0.1:8002')
+})
---
+++ 001、工具--PM2/PM2/app3.js
@@ -0,0 +1,9 @@
+const http = require('http')
+const server = http.createServer()
+
+server.on('request', (req, res) => {
+    res.end('请求成功' + new Date().toLocaleTimeString())
+})
+server.listen(8003, () => {
+    console.log('server running at http://127.0.0.1:8003')
+})
